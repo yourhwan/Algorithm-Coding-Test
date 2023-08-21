7ae564644e08de4f39ad468fdb087c29f7f0a84a
yourhwan <88339276+yourhwan@users.noreply.github.com>
2023-08-21 17:40:23 +0900

    [level 1] Title: 시저 암호, Time: 33.95 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+     
+        for(char ch : s.toCharArray()) {
+            if(ch == ' '){
+                answer += ch;
+            }
+            else if(ch >= 'a' && ch <= 'z') {
+                answer += (char)('a'+(ch + n - 'a') % 26); 
+            }
+            else {
+                answer += (char)('A'+(ch + n - 'A') % 26); 
+            }
+        }// end of for()------------
+        System.out.println(answer);
+        return answer;
+    }
+}
