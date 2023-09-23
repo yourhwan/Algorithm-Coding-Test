b95919aa8cddea5a32c5f260ffe875a49aa9a7ab
yourhwan <88339276+yourhwan@users.noreply.github.com>
2023-09-23 19:37:11 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1/num2;
+        // 산술연산자 /는 몫을 반환, %는 나머지를 반환한다.
+        
+        return answer;
+    }
+}
