c5b6513b38f9d476532d4728564168ce22ab9e99
yourhwan <88339276+yourhwan@users.noreply.github.com>
2023-10-01 20:00:03 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.32 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -5,7 +5,10 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(array);
+        // Arrays.sort(); 는 배열을 오름차순으로 정렬해준다.
+        // 만약 내림차순으로 정렬하고자 한다면, Arrays.sort(array,Collections.reverseOrder());을 하면 된다.
         answer = array[array.length/2];
+        // JAVA에서 /는 소수점 이하 부분이 버려지고 정수부만 나타난다. 때문에 /를 사용하는 것이 옳은 풀이다.
 
         return answer;
     }
